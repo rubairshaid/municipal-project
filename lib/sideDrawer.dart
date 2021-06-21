8006dd67f36699cffaf1986486c7ab54a0e4a819
SaraEdais <saraedais99@gmail.com>
2021-06-21 14:01:44 +0300

    modify main and side drawer files

--- lib/sideDrawer.dart
+++ lib/sideDrawer.dart
@@ -8,67 +8,99 @@ class SideDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-            child : Column(
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 45),
+            child: CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.blue.withOpacity(0.8),
+              child: Icon(Icons.person, color: Colors.white, size: 70),
+            ),
+          ),
+          Expanded(
+            child: ListView(
               children: [
-                Expanded(
-                  child: ListView(
-                    children: [
-                      ListTile(
-                        leading: Icon(Icons.home, color: Colors.blue[500] , size: 30,),
-                        trailing: Text("الرئيسية" , style: TextStyle(fontSize: 18 , fontWeight: FontWeight.bold),),
-                        onTap: (){
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => MainPage(),
-                            ));
-                        },
-                      ),
-                      Divider(thickness: 2),
-                      ListTile(
-                        leading: Icon(Icons.person, color: Colors.blue[500] , size: 30,),
-                        trailing: Text("المعلومات الشخصية" , style: TextStyle(fontSize: 18 , fontWeight: FontWeight.bold),),
-                        onTap: (){
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => PersonalPage(),
-                            ));
-                        },
-                      ),
-                      Divider(thickness: 2),
-                      ListTile(
-                        leading: Icon(Icons.library_books, color: Colors.blue[500] , size: 30,),
-                        trailing: Text("التقارير" , style: TextStyle(fontSize: 18 , fontWeight: FontWeight.bold),),
-                        onTap: (){
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => TasksPage(),
-                            ));
-                        },
-                      ),
-                      Divider(thickness: 2),
-                    ],
+                Divider(thickness: 2),
+                ListTile(
+                  leading: Icon(
+                    Icons.home,
+                    color: Colors.blue[500],
+                    size: 30,
+                  ),
+                  trailing: Text(
+                    "الرئيسية",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  onTap: () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MainPage(),
+                        ));
+                  },
                 ),
-              ),
-                Container(
-                  child: Align(
-                    child: ListTile(
-                      leading :Icon(Icons.fast_forward ,size : 30),
-                      trailing : Text("تسجيل الخروج" , style: TextStyle(fontSize: 18 , fontWeight: FontWeight.bold),),
-                      onTap: (){
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => LogInForm(),
-                            ));
-                      },
-                    ),
-                  )
+                Divider(thickness: 2),
+                ListTile(
+                  leading: Icon(
+                    Icons.person,
+                    color: Colors.blue[500],
+                    size: 30,
+                  ),
+                  trailing: Text(
+                    "المعلومات الشخصية",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  onTap: () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PersonalPage(),
+                        ));
+                  },
                 ),
+                Divider(thickness: 2),
+                ListTile(
+                  leading: Icon(
+                    Icons.library_books,
+                    color: Colors.blue[500],
+                    size: 30,
+                  ),
+                  trailing: Text(
+                    "التقارير",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  onTap: () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => TasksPage(),
+                        ));
+                  },
+                ),
+                Divider(thickness: 2),
               ],
-            ), 
-          );              
+            ),
+          ),
+          Container(
+              child: Align(
+            child: ListTile(
+              leading: Icon(Icons.fast_forward, size: 30),
+              trailing: Text(
+                "تسجيل الخروج",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              onTap: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LogInForm(),
+                    ));
+              },
+            ),
+          )),
+        ],
+      ),
+    );
   }
 }
